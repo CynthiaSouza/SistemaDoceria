49d44ef098aed31eba7351da728bbbc918efa60f
CynthiaSouza <cynthia.souza@dcx.ufpb.br>
2025-04-01 15:45:04 -0300

    Add files via upload

--- DoceriaGUIV2.java
+++ DoceriaGUIV2.java
@@ -3,19 +3,21 @@ import java.awt.*;
 
 public class DoceriaGUIV2 extends JFrame {
     JLabel linha1 , linha2;
-    ImageIcon doceImg = new ImageIcon("/imgs/doce.jpg");
+    ImageIcon doceImg = new ImageIcon(getClass().getResource("/imgs/doce.jpg"));
+    Image imagem = doceImg.getImage().getScaledInstance(400, 300, Image.SCALE_SMOOTH);
+    ImageIcon doceRedimencionado = new ImageIcon(imagem);
     
     public DoceriaGUIV2() {
         setTitle("Doceria Nosso Doce");
         setSize(400, 400);
         setLocationRelativeTo(null);
-        setResizable(false);
-        getContentPane().setBackground(Color.white);
+        setResizable(true);
+        getContentPane().setBackground(Color.PINK);
 
         linha1 = new JLabel("Doceria Nosso Doce", JLabel.CENTER);
-        linha1.setForeground(Color.red);
-        linha1.setFont(new Font("Serif", Font.BOLD, 24));
-        linha2 = new JLabel(doceImg, JLabel.CENTER);
+        linha1.setForeground(Color.BLACK);
+        linha1.setFont(new Font("Cardo", Font.BOLD, 24));
+        linha2 = new JLabel(doceRedimencionado, JLabel.CENTER);
 
         getContentPane().setLayout(new GridLayout(3, 1));
         getContentPane().add(linha1);
---
+++ DoceriaGUIV3.java
@@ -0,0 +1,6 @@
+import javax.swing.*;
+
+public class DoceriaGUIV3 extends JFrame {
+    JLabel linha1, linha2;
+    ImageIcon doceImg = new ImageIcon(getClass().getResource("/imgs/doce.jpg"));
+}
